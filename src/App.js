cf9345b481b4201227081f21295a0e4be7989b55
udittripathi <71835574+udittripathi@users.noreply.github.com>
2024-02-22 17:55:13 +0530

    Navbar

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <div class="h-53">
+      <p class="text-balance text-[#7D7987] pl-[190px] pt-[420px]  font-[300] md:container md:mx-auto text-justify ">Trafalgar provides progressive, and affordable healthcare, accessible on mobile and online for everyone</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
